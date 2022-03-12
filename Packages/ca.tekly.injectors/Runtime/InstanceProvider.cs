219610b0ddfd901db631c020ed9b807fb4b575a5
Matt King <matt@tekly.ca>
2022-03-12 09:28:18 -0800

    Removes NCalc and adds license for ImageMagick

---
+++ Packages/ca.tekly.injectors/Runtime/InstanceProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tekly.Injectors
+{
+    public class InstanceProvider : IInstanceProvider
+    {
+        public Type BaseType { get; private set; }
+        public object Instance { get; private set; }
+
+        public static InstanceProvider Create<T>(T instance)
+        {
+            return new InstanceProvider {
+                BaseType = typeof(T),
+                Instance = instance
+            };
+        }
+    }
+}
