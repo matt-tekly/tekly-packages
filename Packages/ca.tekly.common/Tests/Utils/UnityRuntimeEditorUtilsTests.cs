74b521153efb4b1ba2bbdfa8f5f864aa56dd948c
Matt King <matt@tekly.ca>
2022-03-18 20:47:42 -0700

    Adds automatic calls to MockPlayModeStart and MockPlayModeEnd each time a test is started or finished

---
+++ Packages/ca.tekly.common/Tests/Utils/UnityRuntimeEditorUtilsTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework.Interfaces;
+using Tekly.Common.Utils;
+using Tests.Utils;
+using UnityEngine.TestRunner;
+
+[assembly: TestRunCallback(typeof(UnityRuntimeEditorUtilsTests))]
+
+namespace Tests.Utils
+{
+    public class UnityRuntimeEditorUtilsTests : ITestRunCallback
+    {
+        public void RunStarted(ITest testsToRun) { }
+
+        public void RunFinished(ITestResult testResults) { }
+
+        public void TestStarted(ITest test)
+        {
+            UnityRuntimeEditorUtils.MockPlayModeStart();
+        }
+
+        public void TestFinished(ITestResult result)
+        {
+            UnityRuntimeEditorUtils.MockPlayModeEnd();
+        }
+    }
+}
