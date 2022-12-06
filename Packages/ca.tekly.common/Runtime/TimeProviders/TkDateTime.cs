1dd8ae2653e4aedc26cad39d18a949a2939a8d99
Matt King <matt.king@bluelinestudios.co>
2022-12-06 14:07:00 -0800

    TkDateTime has many utility methods added

--- Packages/ca.tekly.common/Runtime/TimeProviders/TkDateTime.cs
+++ Packages/ca.tekly.common/Runtime/TimeProviders/TkDateTime.cs
@@ -5,36 +5,65 @@ using UnityEngine;
 namespace Tekly.Common.TimeProviders
 {
     [Serializable]
-    public struct TkDateTime : ISerializationCallbackReceiver
+    public struct TkDateTime : ISerializationCallbackReceiver, IComparable, IComparable<TkDateTime>, IEquatable<TkDateTime>
     {
-        public DateTimeOffset Time;
+        public DateTimeOffset Time { get; private set; }
+        
         [SerializeField] private string m_time;
-		
-        public void OnBeforeSerialize()
-        {
-            m_time = Time.ToString(CultureInfo.InvariantCulture);
-        }
+        
+        public int Millisecond => Time.Millisecond;
+        public int Second => Time.Second;
+        public int Minute => Time.Minute;
+        public int Hour => Time.Hour;
+        public int Day => Time.Day;
+        public int Month => Time.Month;
+        
+        public TkDateTime AddMilliseconds(double milliseconds) => Time.AddMilliseconds(milliseconds);
+        public TkDateTime AddSeconds(double seconds) => Time.AddSeconds(seconds);
+        public TkDateTime AddMinutes(double minutes) => Time.AddMinutes(minutes);
+        public TkDateTime AddHours(double hours) => Time.AddMinutes(hours);
+        public TkDateTime AddDays(double days) => Time.AddMinutes(days);
+        public TkDateTime AddMonths(int months) => Time.AddMonths(months);
+        public TkDateTime AddYears(int months) => Time.AddYears(months);
 
+        public TkDateTime Add(TimeSpan timeSpan) => Time.Add(timeSpan);
+        public TkDateTime Subtract(TimeSpan timeSpan) => Time.Subtract(timeSpan);
+
+        public override string ToString() => Time.ToString(CultureInfo.InvariantCulture);
+
+        public void OnBeforeSerialize() => m_time = Time.ToString(CultureInfo.InvariantCulture);
         public void OnAfterDeserialize()
         {
-            DateTimeOffset.TryParse(m_time, out Time);
+            if (!string.IsNullOrEmpty(m_time) && DateTimeOffset.TryParse(m_time, out var time)) {
+                Time = time;
+            }
         }
 
         public bool Equals(TkDateTime other) => Time.Equals(other.Time);
+        public int CompareTo(TkDateTime other) => Time.CompareTo(other.Time);
         public override bool Equals(object obj) => obj is TkDateTime other && Equals(other);
         public override int GetHashCode() => Time.GetHashCode();
 
+        public int CompareTo(object obj)
+        {
+            if (obj is TkDateTime other) {
+                return Time.CompareTo(other.Time);
+            }
+
+            return obj == null ? 1 : 0;
+        }
+
         public static TkDateTime Now => DateTimeOffset.Now;
-		
+
         public static implicit operator TkDateTime(DateTimeOffset dateTime) => new TkDateTime {Time = dateTime};
-        
+
         public static bool operator ==(TkDateTime left, TkDateTime right) => left.Time == right.Time;
         public static bool operator >(TkDateTime left, TkDateTime right) => left.Time > right.Time;
         public static bool operator >=(TkDateTime left, TkDateTime right) => left.Time >= right.Time;
         public static bool operator <=(TkDateTime left, TkDateTime right) => left.Time <= right.Time;
         public static bool operator !=(TkDateTime left, TkDateTime right) => left.Time != right.Time;
         public static bool operator <(TkDateTime left, TkDateTime right) => left.Time < right.Time;
-        
+
         public static TkDateTime operator +(TkDateTime left, TimeSpan right) => left.Time + right;
         public static TkDateTime operator -(TkDateTime left, TimeSpan right) => left.Time - right;
     }
