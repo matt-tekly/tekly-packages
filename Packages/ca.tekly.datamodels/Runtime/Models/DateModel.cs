8bd2a99dbeb721ea4bf9e468659287dd273298d4
Matt King <matt.king@bluelinestudios.co>
2022-12-08 14:15:10 -0800

    Adds DateModel

---
+++ Packages/ca.tekly.datamodels/Runtime/Models/DateModel.cs
@@ -0,0 +1,26 @@
+using System.Text;
+using Tekly.Common.TimeProviders;
+
+namespace Tekly.DataModels.Models
+{
+    public class DateModel : ValueModel<TkDateTime>
+    {
+        public DateModel(TkDateTime value) : base(value) {}
+		
+        public override int CompareTo(IValueModel valueModel)
+        {
+            var other = valueModel as DateModel;
+            return m_value.CompareTo(other.Value);
+        }
+        
+        public override void ToJson(StringBuilder sb)
+        {
+            sb.Append($"\"{Value}\"");
+        }
+
+        public override string ToDisplayString()
+        {
+            return Value.ToString();
+        }
+    }
+}
