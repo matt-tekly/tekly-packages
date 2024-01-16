22f53f1ac67284bf18835082aa7e1b37cfe82390
Matt King <matt@tekly.ca>
2024-01-15 20:20:11 -0800

    Adds DeleteComponent System

---
+++ Packages/ca.tekly.simulantextensions/Runtime/Systems/DeleteComponentSystem.cs
@@ -0,0 +1,26 @@
+using Tekly.Simulant.Core;
+
+namespace Tekly.Simulant.Extensions.Systems
+{
+	/// <summary>
+	/// Deletes component T from all Entities
+	/// </summary>
+	public class DeleteComponentSystem<T> : ITickSystem where T : struct
+	{
+		private readonly DataPool<T> m_dataPool;
+		private readonly Query m_query;
+		
+		public DeleteComponentSystem(World world)
+		{
+			m_dataPool = world.GetPool<T>();
+			m_query = world.Query().Include<T>().Build();
+		}
+		
+		public void Tick(float deltaTime)
+		{
+			foreach (var entity in m_query) {
+				m_dataPool.Delete(entity);
+			}
+		}
+	}
+}
--- Packages/ca.tekly.simulantextensions/Runtime/Systems/SystemsContainer.cs
+++ Packages/ca.tekly.simulantextensions/Runtime/Systems/SystemsContainer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using Tekly.Injectors;
 
 namespace Tekly.Simulant.Extensions.Systems
@@ -10,24 +11,25 @@ namespace Tekly.Simulant.Extensions.Systems
 	{
 		void Tick(float deltaTime);
 	}
-	
+
 	internal class RegisteredSystem
 	{
 		public Type Type;
 		public ISystem System;
 		public bool NeedsInjection;
 	}
-	
+
+	[UsedImplicitly]
 	public class SystemsContainer : IDisposable
 	{
 		private readonly InjectorContainer m_container;
 
-		private List<RegisteredSystem> m_registeredSystems = new List<RegisteredSystem>();
+		private readonly List<RegisteredSystem> m_registeredSystems = new List<RegisteredSystem>();
 
-		private List<ISystem> m_systems = new List<ISystem>();
-		private List<ITickSystem> m_tickSystems = new List<ITickSystem>();
-		private List<IDisposable> m_disposables = new List<IDisposable>();
-		
+		private readonly List<ISystem> m_systems = new List<ISystem>();
+		private readonly List<ITickSystem> m_tickSystems = new List<ITickSystem>();
+		private readonly List<IDisposable> m_disposables = new List<IDisposable>();
+
 		public SystemsContainer(InjectorContainer container)
 		{
 			m_container = container;
@@ -43,15 +45,15 @@ namespace Tekly.Simulant.Extensions.Systems
 					if (registeredSystem.NeedsInjection) {
 						m_container.Inject(registeredSystem.System);
 					}
-					
+
 					m_systems.Add(registeredSystem.System);
 				}
 			}
-			
+
 			Filter(m_systems, m_tickSystems);
 			Filter(m_systems, m_disposables);
 		}
-		
+
 		public void Tick(float deltaTime)
 		{
 			foreach (var tickSystem in m_tickSystems) {
@@ -64,10 +66,21 @@ namespace Tekly.Simulant.Extensions.Systems
 			var pendingSystem = new RegisteredSystem {
 				Type = typeof(T)
 			};
+			
 			m_registeredSystems.Add(pendingSystem);
 			return this;
 		}
 		
+		public SystemsContainer Delete<T>() where T : struct
+		{
+			var pendingSystem = new RegisteredSystem {
+				Type = typeof(DeleteComponentSystem<T>)
+			};
+			
+			m_registeredSystems.Add(pendingSystem);
+			return this;
+		}
+
 		private static void Filter<T>(List<ISystem> systems, List<T> results)
 		{
 			for (int index = 0, count = systems.Count; index < count; index++) {
