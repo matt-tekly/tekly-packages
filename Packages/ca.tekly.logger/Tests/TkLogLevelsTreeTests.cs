b2115ea31f2cfe86e80c5c489f105bf03c3980a2
Matt King <matt@tekly.ca>
2024-07-15 16:14:30 -0700

    Fixes TkLogger tests

--- Packages/ca.tekly.logger/Tests/TkLogLevelsTreeTests.cs
+++ Packages/ca.tekly.logger/Tests/TkLogLevelsTreeTests.cs
@@ -1,71 +1,71 @@
-﻿// using System.Collections.Generic;
-// using NUnit.Framework;
-//
-// namespace Tekly.Logging.Tests
-// {
-//     public class TkLogLevelsTreeTests
-//     {
-//         [Test]
-//         [TestCase("Test", TkLogLevel.Exception)]
-//         [TestCase("TestA.TestB", TkLogLevel.Exception)]
-//         [TestCase("TestA.TestB.TestC", TkLogLevel.Exception)]
-//         public void SimpleTest(string name, TkLogLevel level)
-//         {
-//             var loggersData = new LoggersData {
-//                 Loggers = new List<LoggerSettingsData> {
-//                     new LoggerSettingsData {Logger = name, Level = level},
-//                 }
-//             };
-//
-//             var tree = new LogSettingsTree();
-//             tree.Initialize(loggersData);
-//
-//             Assert.That(tree.GetSettings(name).Level, Is.EqualTo(level));
-//         }
-//
-//         [Test]
-//         [TestCase("Test")]
-//         [TestCase("TestA.TestB")]
-//         [TestCase("TestA.TestB.TestC")]
-//         public void DefaultTest(string name)
-//         {
-//             var defaultLevel = TkLogLevel.Exception;
-//
-//             var loggersData = new LoggersData {
-//                 Default = new LoggerSettingsData {
-//                     Level = defaultLevel
-//                 }
-//             };
-//
-//             var tree = new LogSettingsTree();
-//             tree.Initialize(loggersData);
-//
-//             Assert.That(tree.GetSettings(name).Level, Is.EqualTo(defaultLevel));
-//         }
-//
-//         [Test]
-//         public void SubGroupTest()
-//         {
-//             var defaultLevel = TkLogLevel.Exception;
-//             var targetLevel = TkLogLevel.Debug;
-//             var testDLevel = TkLogLevel.Info;
-//
-//             var loggersData = new LoggersData {
-//                 Default = new LoggerSettingsData {Level = defaultLevel},
-//                 Loggers = new List<LoggerSettingsData> {
-//                     new LoggerSettingsData {Logger = "TestA.TestB", Level = targetLevel},
-//                     new LoggerSettingsData {Logger = "TestA.TestB.TestC.TestD", Level = testDLevel},
-//                 }
-//             };
-//
-//             var tree = new LogSettingsTree();
-//             tree.Initialize(loggersData);
-//
-//             Assert.That(tree.GetSettings("TestA").Level, Is.EqualTo(defaultLevel));
-//             Assert.That(tree.GetSettings("TestA.TestB").Level, Is.EqualTo(targetLevel));
-//             Assert.That(tree.GetSettings("TestA.TestB.TestC").Level, Is.EqualTo(targetLevel));
-//             Assert.That(tree.GetSettings("TestA.TestB.TestC2").Level, Is.EqualTo(targetLevel));
-//             Assert.That(tree.GetSettings("TestA.TestB.TestC.TestD").Level, Is.EqualTo(testDLevel));
-//         }
-//     }
-// }
+﻿using System;
+using NUnit.Framework;
+using Tekly.Logging.Configurations;
+
+namespace Tekly.Logging.Tests
+{
+	public class TkLogLevelsTreeTests
+	{
+		[Test]
+		[TestCase("Test", TkLogLevel.Exception)]
+		[TestCase("TestA.TestB", TkLogLevel.Exception)]
+		[TestCase("TestA.TestB.TestC", TkLogLevel.Exception)]
+		public void SimpleTest(string name, TkLogLevel level)
+		{
+			var loggerConfigs = new[] {
+				new LoggerConfig {
+					Logger = name,
+					Level = level
+				}
+			};
+
+			var tree = new LogSettingsTree();
+			tree.Initialize(TkLogLevel.Debug, (ILogDestination)null, loggerConfigs);
+
+			Assert.That(tree.GetSettings(name).Level, Is.EqualTo(level));
+		}
+
+		[Test]
+		[TestCase("Test")]
+		[TestCase("TestA.TestB")]
+		[TestCase("TestA.TestB.TestC")]
+		public void DefaultTest(string name)
+		{
+			var defaultLevel = TkLogLevel.Exception;
+
+			var tree = new LogSettingsTree();
+			tree.Initialize(defaultLevel, (ILogDestination)null, Array.Empty<LoggerConfig>());
+
+			Assert.That(tree.GetSettings(name).Level, Is.EqualTo(defaultLevel));
+		}
+
+		[Test]
+		public void SubGroupTest()
+		{
+			var defaultLevel = TkLogLevel.Exception;
+			var targetLevel = TkLogLevel.Debug;
+			var testDLevel = TkLogLevel.Info;
+
+			var loggerConfigs = new[] {
+				new LoggerConfig {
+					Logger = "TestA.TestB",
+					Level = targetLevel
+				},
+				new LoggerConfig {
+					Logger = "TestA.TestB.TestC.TestD",
+					Level = testDLevel
+				}
+				
+			};
+
+			var tree = new LogSettingsTree();
+			tree.Initialize(defaultLevel, (ILogDestination)null, loggerConfigs);
+
+			Assert.That(tree.GetSettings("TestA").Level, Is.EqualTo(defaultLevel));
+			Assert.That(tree.GetSettings("TestA.TestB").Level, Is.EqualTo(targetLevel));
+			Assert.That(tree.GetSettings("TestA.TestB.TestC").Level, Is.EqualTo(targetLevel));
+			Assert.That(tree.GetSettings("TestA.TestB.TestC2").Level, Is.EqualTo(targetLevel));
+			Assert.That(tree.GetSettings("TestA.TestB.TestC.TestD").Level, Is.EqualTo(testDLevel));
+		}
+	}
+}
