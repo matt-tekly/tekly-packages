72f2a946960c1dd48dc4defaf915f055576070a8
Matt King <matt@tekly.ca>
2025-06-30 09:06:39 -0700

    Fixes tracing build issue

--- Packages/ca.tekly.tracing/Runtime/PerfettoExporter.cs
+++ Packages/ca.tekly.tracing/Runtime/PerfettoExporter.cs
@@ -1,21 +1,24 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
-using UnityEditor;
 
 namespace Tekly.Tracing
 {
 	public static class PerfettoExporter
 	{
-		[MenuItem("Tools/Tekly/Trace Events", priority = 0)]
+		
+#if UNITY_EDITOR
+		[UnityEditor.MenuItem("Tools/Tekly/Trace Events", priority = 0)]
 		public static void ToTraceEventsMenu()
 		{
 			TraceEvents.ProcessEvents(evts => {
 				var json = ToJson(evts);
-				File.WriteAllText("traceevents.json", json);
+				Directory.CreateDirectory("Traces");
+				File.WriteAllText("Traces/traceevents.json", json);
 			});
 		}
-
+#endif
+		
 		public static string ToJson(List<TraceEvent> source)
 		{
 			var events = PerfettoTraceEvent.Convert(source);
