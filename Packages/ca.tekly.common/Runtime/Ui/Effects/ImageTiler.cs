d23122aa21c6e6f17aad6ca739bba7caba3b172f
Matt King <matt@tekly.ca>
2026-05-26 07:10:57 -0700

    [Common] Image Tiler supports offsets

--- Packages/ca.tekly.common/Runtime/Ui/Effects/ImageTiler.cs
+++ Packages/ca.tekly.common/Runtime/Ui/Effects/ImageTiler.cs
@@ -6,63 +6,65 @@ using UnityEngine.UI;
 
 namespace Tekly.Common.Ui.Effects
 {
-    public class ImageTiler : BaseMeshEffect
-    {
-        [SerializeField] private float m_horizontalScale = 1f;
-        [SerializeField] private float m_verticalScale = 1f;
+	public class ImageTiler : BaseMeshEffect
+	{
+		[SerializeField] private float m_horizontalScale = 1f;
+		[SerializeField] private float m_verticalScale = 1f;
+		[SerializeField] private float m_horizontalOffset;
+		[SerializeField] private float m_verticalOffset;
 		
-        [NonSerialized] private Image m_image;
-        [NonSerialized] private RectTransform m_rectTransform;
+		[NonSerialized] private Image m_image;
+		[NonSerialized] private RectTransform m_rectTransform;
 
-        private Image Image {
-            get {
-                if (m_image == null) {
-                    m_image = GetComponent<Image>();
-                }
+		private Image Image {
+			get {
+				if (m_image == null) {
+					m_image = GetComponent<Image>();
+				}
 
-                return m_image;
-            }
-        }
+				return m_image;
+			}
+		}
 		
-        private RectTransform RectTransform {
-            get {
-                if (m_rectTransform == null) {
-                    m_rectTransform = GetComponent<RectTransform>();
-                }
+		private RectTransform RectTransform {
+			get {
+				if (m_rectTransform == null) {
+					m_rectTransform = GetComponent<RectTransform>();
+				}
 
-                return m_rectTransform;
-            }
-        }
+				return m_rectTransform;
+			}
+		}
 
-        public override void ModifyMesh(VertexHelper vh)
-        {
-            if (!enabled) {
-                return;
-            }
+		public override void ModifyMesh(VertexHelper vh)
+		{
+			if (!enabled) {
+				return;
+			}
 			
-            List<UIVertex> buffer = ListPool<UIVertex>.Get();
-            vh.GetUIVertexStream(buffer);
-            Apply(buffer);
-            vh.AddUIVertexTriangleStream(buffer);
-            ListPool<UIVertex>.Release(buffer);
-        }
+			List<UIVertex> buffer = ListPool<UIVertex>.Get();
+			vh.GetUIVertexStream(buffer);
+			Apply(buffer);
+			vh.AddUIVertexTriangleStream(buffer);
+			ListPool<UIVertex>.Release(buffer);
+		}
 
-        private void Apply(List<UIVertex> verts)
-        {
-            var rect = RectTransform.rect;
-            var spriteRect = Image.sprite.rect;
+		private void Apply(List<UIVertex> verts)
+		{
+			var rect = RectTransform.rect;
+			var spriteRect = Image.sprite.rect;
 			
-            var x = rect.width / spriteRect.width * m_horizontalScale;
-            var y = rect.height / spriteRect.height * m_verticalScale;
+			var x = rect.width / spriteRect.width * m_horizontalScale;
+			var y = rect.height / spriteRect.height * m_verticalScale;
 
-            for (var i = 0; i < verts.Count; ++i) {
-                var v = verts[i];
+			for (var i = 0; i < verts.Count; ++i) {
+				var v = verts[i];
 
-                v.uv0.x *= x;
-                v.uv0.y *= y;
+				v.uv0.x = v.uv0.x * x + m_horizontalOffset;
+				v.uv0.y = v.uv0.y * y + m_verticalOffset;
 
-                verts[i] = v;
-            }
-        }
-    }
+				verts[i] = v;
+			}
+		}
+	}
 }
