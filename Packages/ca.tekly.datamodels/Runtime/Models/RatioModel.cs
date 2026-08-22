005c25eff27fa2e7512412b8ceea8a816c928e7d
Matt King <matt@tekly.ca>
2026-08-22 14:04:20 -0700

    [DataModels] RatioModel API improvements

--- Packages/ca.tekly.datamodels/Runtime/Models/RatioModel.cs
+++ Packages/ca.tekly.datamodels/Runtime/Models/RatioModel.cs
@@ -1,23 +1,60 @@
+using System;
+using Tekly.Common.Maths;
+
 namespace Tekly.DataModels.Models
 {
 	public class RatioModel : ObjectModel
 	{
-		public float Current {
+		public double Ratio => m_ratio.Value;
+		
+		public double Current {
+			get => m_current.Value;
 			set {
+				if (MathUtils.IsApproximately(m_current.Value, value)) {
+					return;
+				}
+				
 				m_current.Value = value;
 				m_ratio.Value = m_current.Value / m_max.Value;
 				
 				m_full.Value = m_ratio.Value >= 1.0f;
+				EmitModified();
 			}
 		}
 
-		public float Max {
+		public double Max {
+			get => m_max.Value;
 			set {
+				if (MathUtils.IsApproximately(m_max.Value, value)) {
+					return;
+				}
+				
 				m_max.Value = value;
 				m_ratio.Value = m_current.Value / m_max.Value;
 
 				m_full.Value = m_ratio.Value >= 1.0f;
+				EmitModified();
 			}
+		}
+		
+		public IDisposable SubscribeToRatio(Action<double> action)
+		{
+			return m_ratio.Subscribe(action);
+		}
+		
+		public IDisposable SubscribeToCurrent(Action<double> action)
+		{
+			return m_current.Subscribe(action);
+		}
+		
+		public IDisposable SubscribeToMax(Action<double> action)
+		{
+			return m_max.Subscribe(action);
+		}
+		
+		public IDisposable SubscribeToFull(Action<bool> action)
+		{
+			return m_full.Subscribe(action);
 		}
 
 		public bool IsFull => m_full.Value; 
